66db63a97cbfe7c33d9415966950697240793d18
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-26 15:24:25 -0400

    Some basic routing

---
+++ src/HomeScreen.js
@@ -0,0 +1,18 @@
+import './HomeScreen.css';
+
+// Import the functions you need from the SDKs you need
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      <header className="HomeScreen-header">
+        <h1>Paper Scout V1</h1>
+        <button>SCOUT</button>
+        <button>LEECH</button>
+        <a>How does this work?</a>
+      </header>
+    </div>
+  );
+}
+
+export default HomeScreen;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import HomeScreen from './HomeScreen';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4ji48pLAuBxBtmJx6DZxarzGPKoZrv8w",
+  authDomain: "paper-scout.firebaseapp.com",
+  projectId: "paper-scout",
+  storageBucket: "paper-scout.appspot.com",
+  messagingSenderId: "771182993262",
+  appId: "1:771182993262:web:e3c6b1311024539c8eeb7f",
+  measurementId: "G-WTE4YWNT0H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen/>} />
+      </Routes>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 

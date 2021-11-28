a206268593ba5a00923a4bd8372c331d8dfbf605
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-28 14:42:18 -0400

    Add a sign out button on the login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,20 +1,32 @@
+import {useState} from 'react'
 import Flexbox from 'flexbox-react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebaseApp from '../FirebaseApp';
-import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, EmailAuthProvider, signOut, onAuthStateChanged} from 'firebase/auth';
 
 const auth = getAuth(firebaseApp)
+
 const uiConfig = {
   signInFlow: 'popup',
   signInSuccessUrl: '/',
   signInOptions: [GoogleAuthProvider.PROVIDER_ID, EmailAuthProvider.PROVIDER_ID],
 };
 
+const signOutClicked = (auth) => {
+  signOut(auth)
+  window.location.replace("/");
+}
+
 function LoginScreen() {
+  const [ currentUser, setCurrentUser ] = useState()
+  onAuthStateChanged(auth, user => setCurrentUser(user))
+  
   return (
     <Flexbox flexDirection='column' flex="1">
-      <p>Please sign-in:</p>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      {currentUser ?
+       <button onClick={() => signOutClicked(auth)}>Sign Out</button> :
+       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      }
     </Flexbox>
   );
 }

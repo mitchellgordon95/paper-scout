038cb84fc21915ff9b31b8d7e0986b59a71d491e
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-28 19:18:20 -0400

    Make search actually work

--- src/screens/ScoutScreen.js
+++ src/screens/ScoutScreen.js
@@ -4,13 +4,6 @@ import { fetchAndParseArxiv } from '../helpers/Arxiv'
 
 const PER_PAGE = 10
 
-const doSearch = async ({searchQuery, category, pageStart, setPageResults, setTotalResults}) => {
-  const searchQueryFrag = searchQuery ? `all:${searchQuery}` : ''
-  const { pageResults, totalResults } = await fetchAndParseArxiv(`search_query=${searchQueryFrag}+cat:${category}&sortBy=lastUpdatedDate&start=${pageStart}&max_results=${PER_PAGE}`)
-  setPageResults(pageResults)
-  setTotalResults(totalResults)
-}
-
 const navigateToPaper = ({paperId}) => {
   window.location.replace(`/paper/arxiv/${paperId}`)
 }
@@ -22,14 +15,24 @@ function ScoutScreen() {
   const [ pageStart, setPageStart ] = useState(0)
   const [ pageResults, setPageResults ] = useState([])
 
+  const doSearch = async () => {
+    const searchQueryFrag = searchQuery ? `all:${searchQuery}` : ''
+    const { pageResults, totalResults } = await fetchAndParseArxiv(`search_query=${searchQueryFrag}+AND+cat:${category}&sortBy=lastUpdatedDate&start=${pageStart}&max_results=${PER_PAGE}`)
+    setPageResults(pageResults)
+    setTotalResults(totalResults)
+  }
+
   // Do a default search with only the category to populate the page.
-  useEffect(() => doSearch({searchQuery, category, pageStart, setPageResults, setTotalResults}), [])
+  useEffect(doSearch, [])
 
   return (
     <Flexbox flexDirection='column' flex="1">
       <Flexbox>
-        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
-        <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+        <form onSubmit={(e) => {e.preventDefault(); doSearch()}}>
+          <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+          <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+          <input type="submit" value="Submit" />
+        </form>
       </Flexbox>
       <Flexbox flexDirection='column'>
         {pageResults.map(entry => {

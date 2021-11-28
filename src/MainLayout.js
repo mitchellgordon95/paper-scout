0bfcaf5e6cc30c2f014f7725c2ec9b6a809d91d1
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-27 23:47:30 -0400

    Nuke github link, put it somewhere nicer later

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,19 +1,12 @@
 import './MainLayout.css';
 import {Outlet} from 'react-router-dom'
 import Flexbox from 'flexbox-react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 // Placeholder, will eventually hold shared header stuff like login links, profile, etc.
 const MainLayout = () => {
   return (
     <Flexbox flexDirection='column' justifyContent='center' alignItems='center' className="MainLayout" minHeight='100vh'>
       <Outlet/>
-      <Flexbox minHeight="50px">
-        <a href="https://github.com/mitchellgordon95/paper-scout">
-          <FontAwesomeIcon icon={faGithub} size="2x"/>
-        </a>
-      </Flexbox>
     </Flexbox>
   );
 }

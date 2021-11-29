021a2249122229d4556a2fd4bff00aa45bbeda33
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-28 21:15:50 -0400

    Rudimentary pagination

--- src/screens/ScoutScreen.js
+++ src/screens/ScoutScreen.js
@@ -11,38 +11,66 @@ const navigateToPaper = ({paperId}) => {
 function ScoutScreen() {
   const [ category, setCategory ] = useState('cs.CL')
   const [ searchQuery, setSearchQuery ] = useState()
-  const [ totalResults, setTotalResults ] = useState()
-  const [ pageStart, setPageStart ] = useState(0)
+  const [ currentPage, setCurrentPage ] = useState(0)
+  const [ totalResults, setTotalResults] = useState()
   const [ pageResults, setPageResults ] = useState([])
 
-  const doSearch = async () => {
+  const doSearch = async ({pageNumber}) => {
     const searchQueryFrag = searchQuery ? `all:${escape(searchQuery)}+AND+` : ''
-    const { pageResults, totalResults } = await fetchAndParseArxiv(`search_query=${searchQueryFrag}cat:${category}&sortBy=lastUpdatedDate&start=${pageStart}&max_results=${PER_PAGE}`)
+    const { pageResults, totalResults } = await fetchAndParseArxiv(`search_query=${searchQueryFrag}cat:${category}&sortBy=lastUpdatedDate&start=${pageNumber * PER_PAGE}&max_results=${PER_PAGE}`)
     setPageResults(pageResults)
     setTotalResults(totalResults)
   }
 
+  const onSearchSubmit = (e) => {
+    e.preventDefault()
+    doSearch({pageNumber: 0})
+    setCurrentPage(0)
+  }
+
+  const searchNextPage = () => {
+    setCurrentPage(currentPage + 1)
+    doSearch({pageNumber: currentPage+1})
+  }
+
+  const searchPrevPage = () => {
+    setCurrentPage(currentPage - 1)
+    doSearch({pageNumber: currentPage-1})
+  }
+
   // Do a default search with only the category to populate the page.
-  useEffect(doSearch, [])
+  useEffect(() => doSearch({pageNumber: 0}), [])
+  const nextPageButton = <button onClick={searchNextPage}>Next</button>
+  const prevPageButton = <button onClick={searchPrevPage}>Prev</button>
 
   return (
-    <Flexbox flexDirection='column' flex="1">
+    <Flexbox flexDirection='column' flex="1" alignItems='flex-start'>
       <Flexbox>
-        <form onSubmit={(e) => {e.preventDefault(); doSearch()}}>
+        <form onSubmit={onSearchSubmit}>
           <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
           <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
           <input type="submit" value="Submit" />
         </form>
       </Flexbox>
+      <div>Results: {totalResults}</div>
+      <div>Page: {currentPage}</div>
+      <Flexbox flexDirection='row'>
+        {currentPage > 0 ?  prevPageButton : ''}
+        {nextPageButton}
+      </Flexbox>
       <Flexbox flexDirection='column'>
         {pageResults.map(entry => {
           return <Flexbox flexDirection='column' justifyContent='flex-start' key={entry.id} onClick={() => navigateToPaper({paperId: entry.id})}>
             <h4>{entry.title}</h4>
-            <p>{entry.authors ? entry.authors.join(", ") : ""}</p>
-            <p>{entry.abstract.slice(0,200) + "..."}</p>
+            <div>{entry.authors ? entry.authors.join(", ") : ""}</div>
+            <div>{entry.abstract.slice(0,200) + "..."}</div>
           </Flexbox>
           })
         }
+      </Flexbox>
+      <Flexbox flexDirection='row'>
+        {currentPage > 0 ?  prevPageButton : ''}
+        {nextPageButton}
       </Flexbox>
     </Flexbox>
   );

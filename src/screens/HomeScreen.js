01e71cd124dc4e90e9fac2ecd6640c037676a9bd
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-11-28 22:17:03 -0400

    useNavigate with the homescreen buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,9 +3,11 @@ import {useState} from 'react'
 import RulesModal from '../modals/RulesModal'
 import Flexbox from 'flexbox-react'
 import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router'
 
 function HomeScreen() {
   const [showModal, setShowModal] = useState(false)
+  const navigate = useNavigate()
 
   return (
       <Flexbox flexDirection='column' justifyContent='center' alignItems='center' flex="1">
@@ -13,8 +15,8 @@ function HomeScreen() {
           <h1>Paper Scout V1</h1>
         </div>
         <Flexbox flexDirection='row' alignItems='center' justifyContent='space-evenly' height='200px' width='200px'>
-          <button><Link to='/scout'>SCOUT</Link></button>
-          <button>LEECH</button>
+          <button onClick={() => navigate('/scout')}>SCOUT</button>
+          <button onClick={() => navigate('/leech')}>LEECH</button>
         </Flexbox>
         <p onClick={() => setShowModal(true)}>How does this work?</p>
         <RulesModal isOpen={showModal} onClose={() => setShowModal(false)}/>

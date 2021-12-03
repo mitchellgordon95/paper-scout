6871c2231118d701df3b02c6b20e20a18a348f41
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-12-02 20:48:11 -0400

    Fix the firebase functions build

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// TODO (mitchg) - irritatingly, Cloud Functions for Firebase does not
+// implement the ES Module syntax from v9 of the firebase package (and
+// v10 of the admin package).
+
+// https://firebase.google.com/docs/admin/migrate-node-v10
+// https://github.com/firebase/firebase-tools/issues/2994
+
+export const endorsePaper = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});

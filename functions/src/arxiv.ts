3525e626bb1acff8600d74efb481c22822403fb6
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-12-02 21:31:00 -0400

    First pass at an endorsement cloud function

---
+++ functions/src/arxiv.ts
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+
+export const fetchAndParseArxiv = async (queryString) => {
+  const resp = await fetch(`https://export.arxiv.org/api/query?${queryString}`)
+  const xmlStr = await resp.text()
+  const parser = new DOMParser();
+  const parsedXML = parser.parseFromString(xmlStr, 'text/xml')
+  const totalResults = parsedXML.getElementsByTagName('opensearch:totalResults')[0].textContent
+  const entries = parsedXML.getElementsByTagName('entry')
+
+  const jsonEntries = Array.from(entries).map(entry => {
+    const authorList = Array.from(entry.getElementsByTagName('author'))
+    const categories = Array.from(entry.getElementsByTagName('category'))
+    return {
+      id: entry.getElementsByTagName('id')[0].textContent.split('/').pop(),
+      title: entry.getElementsByTagName('title')[0].textContent,
+      abstract: entry.getElementsByTagName('summary')[0].textContent,
+      authors: authorList.map(author => author.getElementsByTagName('name')[0].textContent),
+      categories: categories.map(category => category.getAttribute('term')),
+      updatedAt: entry.getElementsByTagName('updated')[0].textContent
+    }
+  })
+
+  return { pageResults: jsonEntries, totalResults }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,47 @@
 import * as functions from "firebase-functions";
+import { initializeApp } from 'firebase-admin/app'
+import { getFirestore} from 'firebase-admin/firestore'
+import { fetchAndParseArxiv } from './arxiv'
 
-// TODO (mitchg) - irritatingly, Cloud Functions for Firebase does not
-// implement the ES Module syntax from v9 of the firebase package (and
-// v10 of the admin package).
+const app = initializeApp()
+const db = getFirestore(app)
 
-// https://firebase.google.com/docs/admin/migrate-node-v10
-// https://github.com/firebase/firebase-tools/issues/2994
+export const endorsePaper = functions.https.onRequest(async (request, response) => {
+  const { paperId, userId } = request.query
 
-export const endorsePaper = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  // TODO - check auth
+  // TODO (mitchg) - irritatingly, Cloud Functions for Firebase does not
+  // implement the nice ES Module syntax from v9 of the firebase package.
+
+  const user = (await db.collection('users').doc(userId).get()).data()
+  const paperInfo = (await fetchAndParseArxiv(`id_list=${paperId}`)).pageResults[0]
+
+  const endorsements = (await db.collection('endorsements')
+    .where('userId', "==", userId)
+    .orderBy('updatedAt')
+    .get()).docs.map(doc => doc.data())
+
+  const currentEndorsements = endorsements.filter(endorsement => !endorsement.deletedAt)
+
+  if (currentEndorsements.length > 4) {
+    response.status(500).send("Too many endorsements.")
+    return
+  }
+
+  if (endorsements.filter(endorsement => endorsement.paperId === paperId)) {
+    response.status(500).send("Paper already endorsed.")
+    return
+  }
+
+  db.collection('endorsements').addDoc({
+    paperId,
+    userId,
+    paperInfo,
+    userDisplayName: user.displayName,
+    updatedAt: Date.now(),
+    deletedAt: null,
+  })
+
+  // Clients using onSnapshot will auto-pull DB changes
+  response.status(200)
 });

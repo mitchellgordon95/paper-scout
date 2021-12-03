038a5cecfe4df9f81e86e50afda03262c93fe3d8
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-12-02 22:18:02 -0400

    Adding point tracking to endorsePaper cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,48 @@
 import functions from "firebase-functions";
 import { initializeApp } from 'firebase-admin/app'
-import { getFirestore} from 'firebase-admin/firestore'
+import { getFirestore, FieldValue } from 'firebase-admin/firestore'
 import { fetchAndParseArxiv } from './arxiv.js'
+
 
 
 const app = initializeApp()
 const db = getFirestore(app)
+const increment = FieldValue.increment(1);
 
 export const endorsePaper = functions.https.onRequest(async (request, response) => {
   const { paperId, userId } = request.query
 
-  // TODO - check auth
-  // TODO (mitchg) - irritatingly, Cloud Functions for Firebase does not
-  // implement the nice ES Module syntax from v9 of the firebase package.
+  // TODO (mitchg) - check auth
 
   const user = (await db.collection('users').doc(userId).get()).data()
   const paperInfo = (await fetchAndParseArxiv(`id_list=${paperId}`)).pageResults[0]
 
-  const endorsements = (await db.collection('endorsements')
+  const userEndorsements = (await db.collection('endorsements')
     .where('userId', "==", userId)
     .orderBy('updatedAt')
     .get()).docs.map(doc => doc.data())
 
-  const currentEndorsements = endorsements.filter(endorsement => !endorsement.deletedAt)
+  const currentEndorsements = userEndorsements.filter(endorsement => !endorsement.deletedAt)
 
   if (currentEndorsements.length > 4) {
     response.status(500).send("Too many endorsements.")
     return
   }
 
-  if (endorsements.filter(endorsement => endorsement.paperId === paperId)) {
+  if (userEndorsements.filter(endorsement => endorsement.paperId === paperId).length > 0) {
     response.status(500).send("Paper already endorsed.")
     return
   }
+  
+  const paperEndorsements = (await db.collection('endorsements')
+    .where('paperId', "==", paperId)
+    .get()).docs.map(doc => doc.data())
 
-  db.collection('endorsements').addDoc({
+  paperEndorsements.filter(endorsement => !endorsement.deletedAt).map(endorsement => {
+    db.collection('users').doc(endorsement.userId).update({points: increment})
+  })
+
+  db.collection('endorsements').add({
     paperId,
     userId,
     paperInfo,
@@ -42,7 +50,8 @@ export const endorsePaper = functions.https.onRequest(async (request, response) 
     updatedAt: Date.now(),
     deletedAt: null,
   })
+  
+  // Clients using onSnapshot will auto-pull DB changes.
+  response.status(200).send('ok')
 
-  // Clients using onSnapshot will auto-pull DB changes
-  response.status(200)
 });

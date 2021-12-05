9d4b5509d53c46e4bdd08dba39d6130ec429d2c4
Mitchell Gordon <mitchell.gordon95@gmail.com>
2021-12-04 20:51:51 -0400

    Add top papers to the leech screen

--- src/screens/LeechScreen.js
+++ src/screens/LeechScreen.js
@@ -1,3 +1,4 @@
+import './LeechScreen.css'
 import { useState, useEffect } from 'react'
 import Flexbox from 'flexbox-react'
 import { useNavigate } from 'react-router'
@@ -14,21 +15,46 @@ const db = getFirestore(firebaseApp)
 function LeechScreen() {
   const navigate = useNavigate()
   const [users, setUsers] = useState()
+  const [papers, setPapers] = useState()
   useEffect(() => {
-    const q = query(collection(db, 'users'), orderBy('points', 'desc'))
-    onSnapshot(q, (snapshot) =>
+    const userQuery = query(collection(db, 'users'), orderBy('points', 'desc'))
+    onSnapshot(userQuery, (snapshot) =>
       setUsers(snapshot.docs.map((x) => ({ id: x.id, ...x.data() })))
+    )
+    const paperQuery = query(
+      collection(db, 'papers'),
+      orderBy('lifetimeEndorsements', 'desc')
+    )
+    onSnapshot(paperQuery, (snapshot) =>
+      setPapers(snapshot.docs.map((x) => ({ id: x.id, ...x.data() })))
     )
   }, [])
 
   return (
-    <Flexbox flexDirection="column" flex="1">
+    <Flexbox className="LeechScreen" flexDirection="row" flex="1" wrap="wrap">
       {users ? (
-        <Flexbox flexDirection="column">
-          <div>Leaderboard:</div>
+        <Flexbox flexDirection="column" flex="1" alignItems="center">
+          <div>Leaderboard</div>
           {users.map((user) => (
             <div key={user.id} onClick={() => navigate(`/user/${user.id}`)}>
               {user.displayName} ({user.points || 0} pts)
+            </div>
+          ))}
+        </Flexbox>
+      ) : (
+        'Loading...'
+      )}
+
+      {papers ? (
+        <Flexbox flexDirection="column" flex="1" alignItems="center">
+          <div>Top Papers</div>
+          <div>(Lifetime Endorsements)</div>
+          {papers.map((paper) => (
+            <div
+              key={paper.id}
+              onClick={() => navigate(`/paper/arxiv/${paper.id}`)}
+            >
+              {paper.title} ({paper.lifetimeEndorsements || 0})
             </div>
           ))}
         </Flexbox>
